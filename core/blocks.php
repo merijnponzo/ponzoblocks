9530966f8ca15ae197a187889d71c26859781cb4
merren <merijn@ponzo.nl>
2020-09-04 12:10:05 +0200

    article block
    article block

--- core/blocks.php
+++ core/blocks.php
@@ -53,6 +53,14 @@ class Blocks{
                 'icon' => 'columns',
                 'script' => false,
             ),
+            array(
+                'name' => 'articleblock',
+                'slug' => 'articleblock',
+                'title' => 'Article block',
+                'description' => 'Create articleblocks',
+                'icon' => 'columns',
+                'script' => false,
+            ),
             /*
             array(
                 'name' => 'tiles',
